f8afbff1f2a06cb4ff61abdfaee28d08a7625801
Renan Morais <renan.morais@multicredito.net.br>
2021-05-24 15:05:11 -0300

    bug fixed

--- js/global.js
+++ js/global.js
@@ -1,7 +1,11 @@
 // Gerar Assinatura
 function gerarFunction() {
-    $('td.nome').text($('#input.nome').val());
+    $('td.nome').text($('input.nome').val());
+
     $('td.cargo').text($('input.cargo').val());
+    
+    $('a.telefone').attr('href', `tel:+55${$('input.telefone').val().replace(/[^0-9]/g, "")}`).text(`${$('input.telefone').val()}`);
+
     $('a.celular').attr('href', `tel:+55${$('input.celular').val().replace(/[^0-9]/g, "")}`).text(`${$('input.celular').val()}`);
 }
 
@@ -12,9 +16,11 @@ const baseurl = () => {
         .slice(0, -1)
         .join('/');
 }
+
 const source = (base, element) => {
     return `${base}/${$(element).attr('src')}`;
 }
+
 $(document).ready(function() {
     $.each($('img'), function(index, element) {
         $(element).attr('src', source(baseurl(), element));
@@ -24,6 +30,7 @@ $(document).ready(function() {
 // Mascara Telefone
 $(document).ready(function(){
     const s=s=>11===s.replace(/\D/g,"").length?"(00) 00000-0000":"(00) 0000-00009";
+
     $(".mask-brphones").mask(s,{
         onKeyPress:function(a,m,e,k){e.mask(s.apply({},arguments),k)}
     })
@@ -32,10 +39,12 @@ $(document).ready(function(){
 // Copiar
 function copyFunction(el = document.getElementById("table")) {
     var body = document.body, range, sel;
+
     if (document.createRange && window.getSelection) {
         range = document.createRange();
         sel = window.getSelection();
         sel.removeAllRanges();
+
         try {
             range.selectNodeContents(el);
             sel.addRange(range);
@@ -60,6 +69,7 @@ function copyToClip(str) {
         e.clipboardData.setData("text/plain", str);
         e.preventDefault();
     }
+    
     document.addEventListener("copy", listener);
     document.execCommand("copy");
     document.removeEventListener("copy", listener);
